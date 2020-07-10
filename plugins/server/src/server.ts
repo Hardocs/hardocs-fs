36e1de287880bbde62bbd23781590642ff35453e
Divine Hycenth <divinehycenth@outlook.com>
2020-07-10 19:03:13 +0100

    🐛 still facing some issues with couchdb

--- plugins/server/src/server.ts
+++ plugins/server/src/server.ts
@@ -2,52 +2,48 @@ import { GraphQLServer } from 'graphql-yoga';
 import { Server } from 'http';
 import { Server as HTTPSServer } from 'https';
 import generateSchema from './utils/generateSchema';
-import * as Nano from 'nano';
-// import * as request from 'request';
+// import * as Nano from 'nano';
 
 export default async (): Promise<Server | HTTPSServer> => {
-  const db_name = 'test';
-  const nano = Nano({
-    url: process.env.DB_HOST_AUTH,
-    log: (id, args) => {
-      console.log(id, args);
-    }
-  });
-  const db = nano.use(db_name);
-  interface iPerson extends Nano.MaybeDocument {
-    name: string;
-    dob: string;
-  }
+  // const db_name = 'test';
+  // const nano = Nano({
+  //   url: process.env.DB_HOST_AUTH,
+  //   log: (id, args) => {
+  //     console.log(id, args);
+  //   }
+  // });
+  // const db = nano.use(db_name);
+  // interface iPerson extends Nano.MaybeDocument {
+  //   name: string;
+  //   dob: string;
+  // }
 
-  class Person implements iPerson {
-    _id: string;
-    _rev: string;
-    name: string;
-    dob: string;
+  // class Person implements iPerson {
+  //   _id: string;
+  //   _rev: string;
+  //   name: string;
+  //   dob: string;
 
-    constructor(name: string, dob: string) {
-      this._id = undefined;
-      this._rev = undefined;
-      this.name = name;
-      this.dob = dob;
-    }
+  //   constructor(name: string, dob: string) {
+  //     this._id = undefined;
+  //     this._rev = undefined;
+  //     this.name = name;
+  //     this.dob = dob;
+  //   }
 
-    processAPIResponse(response: Nano.DocumentInsertResponse) {
-      if (response.ok === true) {
-        this._id = response.id;
-        this._rev = response.rev;
-      }
-    }
-  }
+  //   processAPIResponse(response: Nano.DocumentInsertResponse) {
+  //     if (response.ok === true) {
+  //       this._id = response.id;
+  //       this._rev = response.rev;
+  //     }
+  //   }
+  // }
 
-  const p = new Person('Bob', '2015-02-04');
-  db.insert(p).then((response) => {
-    p.processAPIResponse(response);
-    console.log(p);
-  });
-
-  // request.get('http://nodejs.org/logo.png').pipe(db.attachment.insert('new', 'logo.png', null, 'image/png'));
-
+  // const p = new Person('Bob', '2015-02-04');
+  // db.insert(p).then((response) => {
+  //   p.processAPIResponse(response);
+  //   console.log(p);
+  // });
   const schema = generateSchema();
 
   const server = new GraphQLServer({
