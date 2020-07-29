46e0c26aef9c066e49602f9b714516341cef4656
Divine Hycenth <divinehycenth@outlook.com>
2020-07-29 10:14:26 +0100

    feat: 🚀 Added new template

--- packages/server/src/modules/project/index.ts
+++ packages/server/src/modules/project/index.ts
@@ -5,27 +5,38 @@ import cwd from '../cwd/cwd';
 import folder from '../folder';
 
 const create = async ({ input }: HDS.ICreateProjectOnMutationArguments) => {
-  // console.log(input);
   if (input) {
     const dest = path.join(cwd.get(), input.name);
     await cwd.set(dest);
-    // const inCurrent = input.folder === '.';
-    const dir = path.join(__dirname, '../../../template');
+    const templateDir = path.join(__dirname, '../../../template');
     if (!folder.isDirectory({ path: dest })) {
-      await fs.mkdir(dest);
+      await fs.ensureDir(dest);
     }
     try {
-      if (
-        folder.isDirectory({ path: dir }) &&
-        folder.isDirectory({ path: dest })
-      ) {
-        await fs.copy(dir, dest);
-      }
-      return {
+      const result = {
+        id: Math.round(Math.abs(Math.random() * new Date().getTime())),
         ...input,
-        updatedAt: new Date().toISOString(),
-        id: Math.round(Math.abs(Math.random() * new Date().getTime()))
+        updatedAt: new Date().toISOString()
       };
+
+      const hardocsDir = path.join(dest, '.hardocs');
+      await fs.ensureDir(hardocsDir);
+      const hardocsJson = `${hardocsDir}/hardocs.json`;
+
+      if (folder.isDirectory({ path: templateDir })) {
+        await fs.copy(templateDir, dest);
+      }
+      const stream = fs.createWriteStream(hardocsJson, {
+        encoding: 'utf8',
+        flags: 'w+'
+      });
+
+      stream.write(JSON.stringify(result, null, 2), (err) => {
+        if (err) {
+          console.log(err.message);
+        }
+      });
+      return result;
     } catch (er) {
       throw new Error(er);
     }
