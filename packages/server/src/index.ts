1a153abaa79d060b537f90a84d1be296de7f44b9
Divine Hycenth <divinehycenth@outlook.com>
2020-08-04 16:43:27 +0100

    fix: 🍌 Successfull build and started up server in production mode

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,5 +1,4 @@
 import server from './server';
-console.log(process.env.DB_HOST_AUTH);
 
 server(); // I didn't start the server authomatically
 // cbecause we're gonna need the connection durring test that is why i exported it to this filr
