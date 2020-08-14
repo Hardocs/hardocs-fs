ab7ad56e6448278d9cf69bb99e0f821e66504e0e
Jose Carlos Urra <jose.urra86@gmail.com>
2020-08-14 14:27:43 +0200

    added proper query to gridsome server

--- packages/electron-gridsome/gridsome.server.js
+++ packages/electron-gridsome/gridsome.server.js
@@ -5,9 +5,27 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-module.exports = function(api) {
+module.exports = function (api) {
 	api.loadSource(({ addCollection }) => {
 		// Use the Data Store API here: https://gridsome.org/docs/data-store-api/
+		// const data = await axios.get('http://localhost:4001');
+		// Use the Data Store API here: https://gridsome.org/docs/data-store-api/
+		const data = axios({
+			url: 'http://localhost:4001',
+			method: 'post',
+			data: {
+			  query: `
+				query {
+					cwd
+				}
+				`
+			}
+		  }).then((result) => {
+			console.log(result.data)
+		  });
+
+		  console.log(data);
+
 	});
 
 	api.createPages(({ createPage }) => {
