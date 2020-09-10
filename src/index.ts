f650612d6f2478478d813cbf811e188ba02e714e
Divine Hycenth <divinehycenth@outlook.com>
2020-09-10 13:50:38 +0100

    fix: ✌ moved console.log disabling into server module

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import server from './server';
-import { __PROD__ } from './utils/constants';
 
 server(); // I didn't start the server automatically
 // because we're gonna need the connection during test that is why i exported it to this
-if (__PROD__) {
-  console.log = () => {};
-}
+
 export * from './server';
