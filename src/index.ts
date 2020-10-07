691825e8de7e58db049d004a44e1ec3cf4a04f8c
Divine Hycenth <divinehycenth@outlook.com>
2020-10-07 16:00:33 +0100

    fix: ✌ cleaned up know unused dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { __DEV__ } from './utils/constants';
 
 if (!__DEV__) {
-	/** Disable console.log in production */
-	// console.log = () => {}
+  /** Disable console.log in production */
+  // console.log = () => {}
 }
 export * from './modules';
