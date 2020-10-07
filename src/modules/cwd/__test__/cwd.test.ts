2e59e433a85fe65f80e73adb57ecf0b5c53cf7ab
Divine Hycenth <divinehycenth@outlook.com>
2020-10-07 15:52:21 +0100

    feat: 🍒 completed cwd tests

--- src/modules/cwd/__test__/cwd.test.ts
+++ src/modules/cwd/__test__/cwd.test.ts
@@ -1,9 +1,18 @@
 import cwd from '../';
+const currentDir = process.cwd();
+const newDir = '/home/divine/Desktop'; // FIXME: Modify this to your os 
 
 describe('Current working Directory: -', () => {
   it('returns a path `string` to your current directory', () => {
-    const currentDir = process.cwd();
-
     expect(cwd.get()).toBe(currentDir)
   });
+
+  it("changes the current directory to path specified", async () => {
+    await cwd.set(newDir)
+    expect(currentDir).not.toBe(newDir)
+  })
+
+  it("new specified directory matches current directory", () => {
+    expect(cwd.get()).toBe(newDir)
+  })
 });
