73062423be3b5e2c9a9546870a7ac4ca16cd0b79
Divine Hycenth <divinehycenth@outlook.com>
2020-10-08 12:25:50 +0100

    feat: 🍒 added file.open test

--- src/modules/file/__test__/file.test.ts
+++ src/modules/file/__test__/file.test.ts
@@ -4,7 +4,24 @@ import path from 'path';
 describe('Test for file operations: ', () => {
   const filePath = path.join(__dirname, 'test-file.md');
   it('exists', () => {
-    console.log(filePath);
     expect(file.exists(filePath)).toBeTruthy();
+  });
+
+  it('opens a file and return an object', () => {
+    const openFile = file.openFile({ path: filePath });
+
+    const openFileKeys = {
+      description: '',
+      title: '',
+      content: '',
+      fileName: '',
+      path: ''
+    };
+
+    const sortedFileKeys = Object.keys(openFile).sort();
+    const sortedFileTest = Object.keys(openFileKeys).sort();
+
+    console.log({ sortedFileKeys, sortedFileTest });
+    expect(sortedFileKeys).toEqual(sortedFileTest);
   });
 });
