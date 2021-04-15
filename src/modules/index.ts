557269cf3f997755e1a314e58d6528ef9096fd3b
Divine Hycenth <divinehycenth@outlook.com>
2021-04-15 20:02:46 +0100

    exported metadata module

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,5 +1,6 @@
 export { default as cwd } from './cwd';
 export { default as file } from './file';
+export * from './metadata';
 // export { default as folder } from './folder';
 // export { default as image } from './image';
 export { default as project } from './project';
